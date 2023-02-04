314984fb24f40e2c519a61626f7b43439827c2ce
Quentin <quentinstuchlik@hotmail.com>
2023-02-04 09:27:08 -0600

    Added basic movement to AI and included the basic enemy in the test scene.

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BossAI : MonoBehaviour
 {
-    
+    public Transform eyes;
+    public bool pace = false;
+
+    public int howLikelyToMove = 10;
+
+    private int direction = -1;
+
+    private int decisiveness = 2;
+    private float howLongIDecided = 2f;
+    private bool moving = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +25,74 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {    
-        for (float i = 0; i < 360; i++)
+
+    }
+
+    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
+    void FixedUpdate()
+    {
+
+        Vector3 fwd = transform.TransformDirection(Vector3.left);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, fwd, out hit, 10))
         {
-            var currentPointPosition = Quaternion.AngleAxis(i, transform.up) * transform.forward;
-            Debug.DrawRay(this.gameObject.transform.position, currentPointPosition, Color.red, 1.0f);
+            Debug.DrawRay(transform.position, fwd * hit.distance, Color.red);
+            //Debug.Log("I see " + hit.transform.gameObject.name);
+            Debug.Log(hit.transform.gameObject.layer);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, fwd * 10, Color.green);
+        }
+        howLongIDecided -= Time.deltaTime;
+        if(howLongIDecided < 0)
+        {
+            int decision = (int)Random.Range(0, 5);
+            Debug.Log("I decided this..." + decision);
+            if(decision == decisiveness)
+            {
+                Debug.Log("Moving");
+                moving = true;
+            }
+            else
+            {
+                Debug.Log("Not Moving");
+                moving = false;
+            }
+            howLongIDecided = Random.Range(0, 2);
+        }
+        if(moving || pace)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        RaycastHit hit;
+        Vector3 fwdDown = transform.TransformDirection(new Vector3(-1, -.4f, 0));
+        if (Physics.Raycast(transform.position, fwdDown, out hit, 1.5f))
+        {
+            Debug.DrawRay(transform.position, fwdDown * hit.distance, Color.red);
+            //Debug.Log("I see " + hit.transform.gameObject.name);
+            Debug.Log(hit.transform.gameObject.layer);
+            if(hit.transform.gameObject.layer == 3)
+            {
+                this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(-10 * direction, 0, 0);
+            }
+            else
+            {
+                this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            }
+        }
+        else
+        {
+            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            Debug.DrawRay(transform.position, fwdDown * 1.5f, Color.green);
+            if(direction < 0) this.gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+            if(direction > 0) this.gameObject.transform.rotation = new Quaternion(0, 180 , 0, 0);
+            direction = -direction;
         }
     }
 }
